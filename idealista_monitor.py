ccd501734f4395f4c7e9d2d53d6fcb7ef58ee4a1
DaybarT <aybar69@gmail.com>
2025-06-02 19:25:20 +0200

    ide

--- idealista_monitor.py
+++ idealista_monitor.py
@@ -11,8 +11,8 @@ import os
 load_dotenv()
 
 
-webhook_url = os.getenv("DISCORD_WEBHOOK")
-intervalo = int(os.getenv("INTERVALO_SEGUNDOS"))
+webhook_url = "https://discord.com/api/webhooks/1379115840754028695/mX6rkpGsnNYStnIkjwEzR3PRK9sF9QYa7g9dmnb0qeKX_Brhi59Ili03oAF4lLUbwcIz"
+intervalo = 1800
 
 URL = "https://www.idealista.com/venta-viviendas/alovera-guadalajara/?ordenado-por=fecha-publicacion-desc"
 HEADERS = {
